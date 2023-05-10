b163c494d4e904cbe8f7db9589669fb59046227a
hyeonwoojeong <qhsh9713@gmail.com>
2023-05-11 00:42:10 +0900

    feature: subpanel vertically

--- 05-with-addtional-UI/floating-panel.jsx
+++ 05-with-addtional-UI/floating-panel.jsx
@@ -9,28 +9,39 @@ var tab = window.add("tabbedpanel", undefined, "")
 var tab1 = tab.add("tab", undefined, "Tab 1");
 var tab2 = tab.add("tab", undefined, "Tab 2");
 
-// Tab1
-var text = tab1.add("statictext", undefined, "Some Example Text");
+// Sub panel in tab1 vertically
+var subPanel = tab1.add("panel", undefined, "Sub Panel");
+subPanel.orientation = "column";
+subPanel.alignChildren = "left";
+subPanel.alignment = "left";
 
-var buttonGroup = tab1.add("group", undefined, "Button Group");
-buttonGroup.orientation = "row";
+
+// Sub tab in tab2
+var subTab2 = tab2.add("tabbedpanel", undefined, "")
+var subTab21 = subTab2.add("tab", undefined, "Sub Tab 1");
+var subTab22 = subTab2.add("tab", undefined, "Sub Tab 2");
+
+// Add button in sub panel
+var buttonGroup = subPanel.add("group", undefined, "Button Group");
+buttonGroup.orientation = "column";
 var button1 = buttonGroup.add("button", undefined, "Button 1");
 var button2 = buttonGroup.add("button", undefined, "Button 2");
+var button3 = buttonGroup.add("button", undefined, "Button 3");
+var button4 = buttonGroup.add("button", undefined, "Button 4");
 
+// Tab2
+var text = tab2.add("statictext", undefined, "Some Example Text");
 
-var dropdown = tab1.add("dropdownlist", undefined, ["Item 1", "Item 2", "Item 3"]);
+var dropdown = tab2.add("dropdownlist", undefined, ["Item 1", "Item 2", "Item 3"]);
 dropdown.size = [150, 25];
 dropdown.selection = 0;
 dropdown.add("item", "Item 4");
 
-
-// Tab2
 var boxesPanel = tab2.add("panel", undefined, "Boxes");
 boxesPanel.orientation = "row";
 var radio = boxesPanel.add("radiobutton", undefined, "Radio Text");
 var checkbox = boxesPanel.add("checkbox", undefined, "Checkbox Text");
 var slider = tab2.add("slider", undefined, "");
-
 
 button1.onClick = function () {
     alert("Button 1 Clicked");
